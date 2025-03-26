1ab24986684be8b761147935809c513d6d000682
developerTamanna <astamanna57@gmail.com>
2025-03-26 20:25:39 +0600

    in out

--- script/time-in-out.js
+++ script/time-in-out.js
@@ -1,1 +1,13 @@
 // alert()
+
+const timerDisplay = document.getElementById("timer");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+startBtn.addEventListener('click', ()=>{
+//   console.log('hello')
+})
+
+stopBtn.addEventListener('click', ()=>{
+    // console.log('stop')
+})
